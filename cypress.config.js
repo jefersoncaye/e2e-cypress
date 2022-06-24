eebabc515a3aab6c3f3de0f0f5f9f93db9d3b30a
Jeferson Fabiano Caye <jeferson.caye@questor.local>
2022-06-24 17:51:19 -0300

    Aula 15. 5.2

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      baseUrl: "https://notes-serverless-app.com",
+      defaultCommandTimeout: 10000,
+      requestTimeout: 10000
   },
 });
